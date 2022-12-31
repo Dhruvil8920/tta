7de5d399431011179deff33786cd1dd65b052ff3
dhruv <gadoyadhruvil0@gmail.com>
2022-12-31 12:20:08 +0530

    added changes

--- AudioResponse.py
+++ AudioResponse.py
@@ -3,5 +3,6 @@ a={
             "how are you":"i am fine.",
             "who are you":"i am alpha's personal ai",
             "what is the time right now":"why don't you look at in watch",
-            "bye":"bye bye."
+            "bye":"bye bye.",
+            "what is your favourite series":"my favourite series is friends."
     }
