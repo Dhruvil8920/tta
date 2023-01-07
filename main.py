815fbdb01569e780484aa41bba340c2faff41443
dhruv <gadoyadhruvil0@gmail.com>
2023-01-07 11:31:25 +0530

    added changes

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-import texttoaudio
-f = open(texttoaudio)
-f.read()
-f.close()
-
+#  1st program
+from playsound import playsound 
+from gtts import gTTS
+from AudioResponse import a
+audio = 'speech.mp3'
+language = 'en'
+print("enter your text")
+b = "hello"
+c = a[b]
+Text = c
+sp = gTTS(text=Text, lang=language, slow=False)
+sp.save(audio)
+playsound(audio)
